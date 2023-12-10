7ba596defaf47bd2f2ce5dae0c53bd483739e008
Kevin <kevin.mavier@hotmail.com>
2023-12-10 01:56:31 +0100

    test fixed branch

---
+++ components/Login.js
@@ -0,0 +1,15 @@
+import styles from '../styles/Login.module.css';
+
+function Login() {
+  return (
+    <div>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          yo to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+      </main>
+    </div>
+  );
+}
+
+export default Login;
