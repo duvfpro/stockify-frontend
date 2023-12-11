02d9f17d117dd83b1b8d0880ac85fe6a6b030e8c
Your name <Your email>
2023-12-11 14:26:25 +0100

    tableau sales + fetch en cours

--- components/LastSales.js
+++ components/LastSales.js
@@ -6,23 +6,30 @@ const columns = [
       title: 'Product',
       width: 120,
       dataIndex: 'product',
-      name: 'product',
+      sorter: true,
     },
+    {
+        title: 'Category',
+        width: 120,
+        dataIndex: 'category',
+        sorter: true,
+      },
     {
       title: 'Date',
       width: 120,
       dataIndex: 'date',
-      sorter: true,
     },
     {
       title: 'Stock left',
       dataIndex: 'stock',
       width: 120,
+      sorter: true,
     },
     {
         title: 'Number of sales',
         dataIndex: 'sales',
         width: 120,
+        sorter: true,
       },
   ];
 
@@ -42,13 +49,13 @@ const columns = [
     //     sales: 2,
     // },
 
-    fetch('http://localhost:3000/allProducts')
-    .then(response => response.json())
-    .then(data => {
-        const date = new Date();
-        console.log(data.soldAt);
-        data.filter(products => products.soldAt.date.includes(date))
-    })
+    // fetch('http://localhost:3000/allProducts')
+    // .then(response => response.json())
+    // .then(data => {
+    //     const date = new Date();
+    //     console.log(data.soldAt);
+    //     data.filter(products => products.soldAt.date.includes(date))
+    // })
   ];
 
 
