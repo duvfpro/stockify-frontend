5cd13f6f64a708b5dcb41ea91c42d23e32b76f8a
Your name <Your email>
2023-12-11 12:12:09 +0100

    creation des pages + header Home

---
+++ components/ProductsPage.js
@@ -0,0 +1,15 @@
+import styles from '../styles/ProductsPage.module.css';
+
+
+function ProductsPage() {
+
+    return (
+        <div>
+            <h1>ProductsPage</h1>
+        </div>
+    )
+
+};
+
+
+export default ProductsPage;
---
+++ pages/statistics.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+import StatsPage from '../components/StatsPage';
+
+function Statistics() {
+  return (
+    <>
+      <Head>
+        <title>Statistics</title>
+      </Head>
+      <StatsPage/>
+    </>
+  );
+}
+
+export default Statistics;
