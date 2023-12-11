38040d9287193e1a9b109cfc2c1b36b5f43583a7
Your name <Your email>
2023-12-11 10:40:47 +0100

    drawerLeft

--- components/Home.js
+++ components/Home.js
@@ -1,15 +1,34 @@
 import styles from '../styles/Home.module.css';
+import DrawerLeft from './DrawerLeft';
+import { CaretRightOutlined  } from '@ant-design/icons';
 
-function Login() {
+import React, { useState } from 'react';
+
+function Home() {
+
+  const [isDrawerOpen, setIsDrawerOpen] = useState(true);
+
+
+  const handleDrawerClick = () => {
+    if(isDrawerOpen) {
+      setIsDrawerOpen(false);
+    } else {
+      setIsDrawerOpen(true);
+    }
+  };
+
+
   return (
     <div>
       <main className={styles.main}>
         <h1 className={styles.title}>
           Click here to see <a href="https://trello.com/b/6LNqv4qE/stockify">the planning</a>
         </h1>
+        <DrawerLeft isDrawerOpen={isDrawerOpen} handleDrawerClick={handleDrawerClick} />
+        <CaretRightOutlined onClick={handleDrawerClick} className={styles.drawerOpenLogo} />
       </main>
     </div>
   );
 }
 
-export default Login;
+export default Home;
