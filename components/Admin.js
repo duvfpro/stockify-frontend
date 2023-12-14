3692280cc9b9392d3c0dac09f266253fe4e15517
Kevin <kevin.mavier@hotmail.com>
2023-12-14 23:52:35 +0100

    commit admin page

--- components/Admin.js
+++ components/Admin.js
@@ -1,27 +1,24 @@
-import { useEffect, useState } from 'react';
-
+import React, { useEffect, useState } from 'react';
+import { Avatar, List, Button, Modal, Input, Switch } from 'antd';
 import styles from '../styles/Pages/Admin.module.css';
-import { Table, Modal, Switch } from 'antd';
-
-import { useSelector } from 'react-redux';
 import { useRouter } from 'next/router';
+import { useSelector } from "react-redux";
 
-function Admin() {
+const { Search } = Input;
+
+const Admin = () => {
   const [userData, setUserData] = useState([]);
   const [selectedUser, setSelectedUser] = useState(null);
-  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
-  const [isNewUserModalOpen, setIsNewUserModalOpen] = useState(false);
-  const [refreshData, setRefreshData] = useState(false);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [editedUsername, setEditedUsername] = useState('');
+  const [createUserPassword, setCreateUserPassword] = useState('');
+  const [editedEmail, setEditedEmail] = useState('');
+  const [editedIsAdmin, setEditedIsAdmin] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [createUserModalVisible, setCreateUserModalVisible] = useState(false);
+
   const user = useSelector((state) => state.user.value);
   const router = useRouter();
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [email, setEmail] = useState('');
-  const [isAdmin, setIsAdmin] = useState(false);
-
-  /////////////////////////////////////////
-  //             TOKEN USER              //
-  /////////////////////////////////////////
 
   useEffect(() => {
     if (!user.token) {
@@ -29,43 +26,12 @@ function Admin() {
     }
   }, [user.token, router]);
 
-  /////////////////////////////////////////
-  //          FORMATING ARRAY            //
-  /////////////////////////////////////////
-
-  const columns = [
-    {
-      title: 'Username',
-      width: 120,
-      dataIndex: 'username',
-    },
-    {
-      title: 'Email',
-      width: 120,
-      dataIndex: 'email',
-    },
-    {
-      title: 'Admin',
-      width: 120,
-      dataIndex: 'isAdmin',
-    },
-    {
-      title: 'Edit',
-      dataIndex: 'edit',
-      width: 120,
-      render: (_, user) => <a className={styles.editButton} onClick={() => openEditModal(user)}>EDIT</a>,
-    },
-  ];
-
-  /////////////////////////////////////////
-  //         FETCHING ALL USER           //
-  /////////////////////////////////////////
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch('http://localhost:3000/users/allUser');
         if (!response.ok) {
-          throw new Error(`Error : ${response.status}`)
+          throw new Error(`Error: ${response.status}`);
         }
         const data = await response.json();
 
@@ -76,162 +42,271 @@ function Admin() {
           isAdmin: user.isAdmin.toString(),
         }));
         setUserData(formattedData);
-
       } catch (error) {
         console.error('Erreur lors du fetch des données : ', error);
       }
     };
 
     fetchData();
-  }, [refreshData]);
+  }, []);
 
-  /////////////////////////////////////////
-  //               EDITION               //
-  /////////////////////////////////////////
-
-  //OPEN EDITION MODAL
-  const openEditModal = (userData) => {
-    setSelectedUser(userData);
-    // Update isAdmin when a new user is selected
-    // converts the character string userData.isAdmin into a boolean.
-    // This should ensure that isAdmin is correctly initialised as a boolean
-    setIsAdmin(userData.isAdmin === 'true');
-    setIsEditModalOpen(true);
+  const handleEditClick = (key) => {
+    const userToEdit = userData.find((user) => user.key === key);
+    setSelectedUser(userToEdit);
+    setEditedUsername(userToEdit.username);
+    setEditedEmail(userToEdit.email);
+    setEditedIsAdmin(userToEdit.isAdmin === 'true');
+    setModalVisible(true);
   };
 
-  //CLOSE EDITION MODAL
-  const closeEditModal = () => {
-    setSelectedUser(null);
-    setIsEditModalOpen(false);
+  const handleModalClose = () => {
+    setModalVisible(false);
   };
 
-  //Function to swith an user to a admin or not
-  const handleEditSwitchChange = () => {
-    setIsAdmin(!isAdmin);
-  }
-
-  //Delete USER
-  const handleDeleteButton = (emailToDelete) => {
-    const isConfirmed = window.confirm("Are you sure you want to delete this user ?");
-    if (isConfirmed) {
-      fetch(`http://localhost:3000/users/${emailToDelete}`, {
-        method: 'DELETE',
+  const handleSaveChanges = async () => {
+    try {
+      const response = await fetch(`http://localhost:3000/users/updateUser/${selectedUser.key}`, {
+        method: 'PUT',
         headers: { 'Content-type': 'application/json' },
-      })
-        .then(response => response.json())
-        .then(() => {
-          setRefreshData(!refreshData);
-          closeEditModal();
-        });
-    };
-  };
-
-  //Save USER Modification
-  const handleEditSaveButton = () => {
-    const fetchUserId = async () => {
-      try {
-
-        const response = await fetch(`http://localhost:3000/users/updateUser/${selectedUser.key}`, {
-          method: 'PUT',
-          headers: { 'Content-type': 'application/json' },
-          body: JSON.stringify({
-            // Use the new isAdmin state variable
-            // to get instant value of admin
-            isAdmin: isAdmin,
-            username: selectedUser.username,
-            email: selectedUser.email,
-          }),
-        });
-
-        const data2 = await response.json();
-
-        setRefreshData(!refreshData);
-        closeEditModal();
-      } catch (error) {
-        console.error('Erreur lors du fetch des données du user: ', error);
-      }
-      console.log(selectedUser);
-    };
-    fetchUserId();
-  };
-
-  /////////////////////////////////////////
-  //              NEW USER               //
-  /////////////////////////////////////////
-
-  //OPEN NEW USER
-  const openNewUserModal = (userData) => {
-    setIsNewUserModalOpen(true);
-  };
-
-  //CLOSE NEW USER
-  const closeNewUserModal = () => {
-    setIsNewUserModalOpen(false);
-  };
-
-  // Save New USER
-  const handleNewUserSaveButton = () => {
-    fetch('http://localhost:3000/users/addUser', {
-      method: 'POST',
-      headers: { 'Content-type': 'application/json' },
-      body: JSON.stringify({ username: username, email: email, isAdmin: isAdmin, password: password }),
-    })
-      .then(response => response.json())
-      .then(() => {
-        setRefreshData(!refreshData);
-        closeNewUserModal();
+        body: JSON.stringify({
+          isAdmin: editedIsAdmin,
+          username: editedUsername,
+          email: editedEmail,
+        }),
       });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data.message);
+
+        const updatedUserData = userData.map((user) =>
+          user.key === selectedUser.key
+            ? {
+              ...user,
+              isAdmin: editedIsAdmin.toString(),
+              username: editedUsername,
+              email: editedEmail,
+            }
+            : user
+        );
+
+        setUserData(updatedUserData);
+      } else {
+        throw new Error(`Error: ${response.status}`);
+      }
+    } catch (error) {
+      console.error('Erreur lors de la mise à jour du user: ', error);
+    } finally {
+      setModalVisible(false);
+    }
   };
 
-  /////////////////////////////////////////
-  //               RETURN                //
-  /////////////////////////////////////////
+  const handleDeleteUser = async () => {
+    const isConfirmed = window.confirm('Are you sure you want to delete this user?');
+    if (isConfirmed) {
+      try {
+        const response = await fetch(`http://localhost:3000/users/${selectedUser.email}`, {
+          method: 'DELETE',
+        });
+
+        if (response.ok) {
+          const data = await response.json();
+          console.log(data.message);
+
+          const updatedUserData = userData.filter((user) => user.key !== selectedUser.key);
+
+          setUserData(updatedUserData);
+        } else {
+          throw new Error(`Error: ${response.status}`);
+        }
+      } catch (error) {
+        console.error('Erreur lors de la suppression du user: ', error);
+      } finally {
+        setModalVisible(false);
+      }
+    }
+  };
+
+  const handleSearch = (value) => {
+    setSearchTerm(value);
+  };
+
+  const filteredData = userData.filter((user) =>
+    user.username.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  const handleCreateUserClick = () => {
+    setEditedUsername('');
+    setEditedEmail('');
+    setEditedIsAdmin(false);
+    setCreateUserModalVisible(true);
+  };
+
+  const handleCreateUserModalClose = () => {
+    setCreateUserModalVisible(false);
+  };
+
+  const handleCreateUser = async () => {
+    try {
+      const response = await fetch('http://localhost:3000/users/addUser', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          username: editedUsername,
+          password: createUserPassword,
+          email: editedEmail,
+          isAdmin: editedIsAdmin,
+        }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+
+        setCreateUserModalVisible(false);
+        window.location.reload();
+      } else {
+        throw new Error(`Error: ${response.status}`);
+      }
+    } catch (error) {
+      console.error('Erreur lors de la création du user: ', error);
+    }
+  };
 
   return (
-    <div>
-      <main className={styles.main}>
-        <button onClick={() => openNewUserModal()} >
-          ADD NEW USER
-        </button>
+    <div className={styles.container}>
+      <div className={styles.searchBar}>
+        <Search
+          placeholder="Search users..."
+          allowClear
+          onSearch={handleSearch}
+          style={{ marginBottom: 16, marginTop: 16 }}
+        />
+        <Button type="primary" onClick={handleCreateUserClick}>
+          Create User
+        </Button>
+      </div>
+      <div className={styles.customListContainer}>
+        <List
+          dataSource={filteredData}
+          pagination={{
+            position: 'bottom',
+            align: 'center',
+          }}
+          renderItem={(item) => (
+            <List.Item className={styles.listItem}>
+              <List.Item.Meta
+                avatar={
+                  <Avatar src={`https://xsgames.co/randomusers/avatar.php?g=pixel&key=${item.key}`} />
+                }
+                title={`${item.username}`}
+                description={`Email: ${item.email}, isAdmin: ${item.isAdmin}`}
+              />
+              <Button type="primary" className={styles.editButton} onClick={() => handleEditClick(item.key)}>
+                Edit
+              </Button>
+            </List.Item>
+          )}
+        />
+      </div>
+      {selectedUser && (
         <Modal
-          title="New User"
-          open={isNewUserModalOpen}
-          onCancel={closeNewUserModal}
+          title={`User information`}
+          visible={modalVisible}
+          onCancel={handleModalClose}
           footer={[
-            <button key="createUserButton" onClick={handleNewUserSaveButton}>
-              Create new User
-            </button>,
+            <Button key="delete" type="primary" danger onClick={handleDeleteUser}>
+              Delete
+            </Button>,
+            <Button key="save" type="primary" onClick={handleSaveChanges}>
+              Save Changes
+            </Button>,
           ]}
         >
-          <p>Username <input onChange={(e) => setUsername(e.target.value)} value={username} /> </p>
-          <p>Email <input onChange={(e) => setEmail(e.target.value)} value={email} /> </p>
-          <p>Password <input onChange={(e) => setPassword(e.target.value)} value={password} /> </p>
-          <p>Admin <Switch checked={selectedUser && selectedUser.isAdmin} onChange={handleEditSwitchChange} size='small' /> </p>
+          <div className={styles.modalField}>
+            <label htmlFor="editedUsername">Username:</label>
+            <Input
+              id="editedUsername"
+              value={editedUsername}
+              onChange={(e) => setEditedUsername(e.target.value)}
+            />
+          </div>
+          <div className={styles.modalField}>
+            <label htmlFor="editedEmail">Email:</label>
+            <Input
+              id="editedEmail"
+              value={editedEmail}
+              onChange={(e) => setEditedEmail(e.target.value)}
+            />
+          </div>
+          <div className={styles.switchContainer}>
+            <label htmlFor="editedIsAdmin">isAdmin:</label>
+            <div className={styles.switchField}>
+              <Switch
+                id="editedIsAdmin"
+                checked={editedIsAdmin}
+                onChange={(checked) => setEditedIsAdmin(checked)}
+              />
+            </div>
+          </div>
         </Modal>
+      )}
+      {createUserModalVisible && (
+        <Modal
+          title={`Create User`}
+          visible={createUserModalVisible}
+          onCancel={handleCreateUserModalClose}
+          footer={[
+            <Button key="cancel" onClick={handleCreateUserModalClose}>
+              Cancel
+            </Button>,
+            <Button key="create" type="primary" onClick={handleCreateUser}>
+              Create
+            </Button>,
+          ]}
+        >
+          <div className={styles.modalField}>
+            <label htmlFor="editedUsername">Username:</label>
+            <Input
+              id="editedUsername"
+              value={editedUsername}
+              onChange={(e) => setEditedUsername(e.target.value)}
+            />
+          </div>
+          <div className={styles.modalField}>
+            <label htmlFor="editedEmail">Email:</label>
+            <Input
+              id="editedEmail"
+              value={editedEmail}
+              onChange={(e) => setEditedEmail(e.target.value)}
+            />
+          </div>
+          <div className={styles.modalField}>
+            <label htmlFor="createUserPassword">Password:</label>
+            <Input
+              id="createUserPassword"
+              type="password"
+              value={createUserPassword}
+              onChange={(e) => setCreateUserPassword(e.target.value)}
+            />
+          </div>
+          <div className={styles.switchContainer}>
+            <label htmlFor="editedIsAdmin">isAdmin:</label>
+            <div className={styles.switchField}>
+              <Switch
+                id="editedIsAdmin"
+                checked={editedIsAdmin}
+                onChange={(checked) => setEditedIsAdmin(checked)}
+              />
+            </div>
+          </div>
+        </Modal>
+      )}
 
-        <div className={styles.tableContainer}>
-          <Table dataSource={userData} columns={columns} pagination={false} style={{ overflow: 'auto', maxHeight: '100%' }} />
-        </div>
-        {isEditModalOpen && (
-          <Modal title="User Information" open={isEditModalOpen} onCancel={closeEditModal}
-
-            footer={[
-              <button onClick={() => handleEditSaveButton()}>
-                Save
-              </button>,
-            ]}
-          >
-            <p> Username <input onChange={(e) => setSelectedUser({ ...selectedUser, username: e.target.value })} value={selectedUser.username} /> </p>
-            <p>Email <input onChange={(e) => setSelectedUser({ ...selectedUser, email: e.target.value })} value={selectedUser.email} /> </p>
-            <p>Admin <Switch onChange={handleEditSwitchChange} defaultChecked={JSON.parse(selectedUser.isAdmin)} size='small' /> </p>
-            <button onClick={() => handleDeleteButton(selectedUser.email)}>
-              DELETE USER
-            </button>
-          </Modal>
-        )}
-      </main>
     </div>
   );
-}
+};
 
 export default Admin;
