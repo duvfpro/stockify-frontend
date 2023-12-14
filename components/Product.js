6b1602bb714474f039c3cff68c59266826b6f410
Your name <Your email>
2023-12-14 11:32:41 +0100

    ajout fonctionnalité edit sur productpage + settimeout sur le useeffect

--- components/Product.js
+++ components/Product.js
@@ -7,13 +7,17 @@ function Product (props) {
         props.handleDeleteButton(props.name);
     };
 
+    const handleEditButton = () => {
+        props.handleEditButton(props.name);
+    }
+
     return (
     <div className={styles.productContainer} >
         <h3 className={styles.nomProduit}>Produit: {props.name}</h3>
         <p className={styles.stock}>Stock: {props.stock}</p>
         <p className={styles.category}>Category: {props.category}</p>
-        <button className={styles.edit}>EDIT</button>
-        <button className={styles.delete} onClick={() => handleDeleteButton()} >DELETE</button>
+        <button className={styles.edit} onClick={() => handleEditButton()} > EDIT </button>
+        <button className={styles.delete} onClick={() => handleDeleteButton()} > DELETE </button>
     </div>
     );
 }
