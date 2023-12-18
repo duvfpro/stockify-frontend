06a24d9e0342fdb0bcdc5d5460a24643cee13736
Your name <Your email>
2023-12-18 12:50:02 +0100

    filtres products

--- components/Tools/FilterCascader.js
+++ components/Tools/FilterCascader.js
@@ -1,47 +1,41 @@
 import React, {useEffect, useState} from 'react';
-import { Cascader } from 'antd';
 import { Select } from 'antd';
 
-const FilterCascader = () => {
+const FilterCascader = (props) => {
     
-const OPTIONS = ['Apples', 'Nails', 'Bananas', 'Helicopters'];
 const [categoriesTab, setCatgoriesTab] = useState([]);
+const [categoriesTabObject, setCategoriersTabObject] = useState([]);
 const [selectedItems, setSelectedItems] = useState([]);
 
 useEffect(() => {
        fetch('http://localhost:3000/categories/allCategories')
         .then(response => response.json())
         .then(data => {
-            let categories=[]
-        for(let i=0; i<data.allCategories.length; i++) {
-            categories.push(data.allCategories[i].name)
-        }
+          let categories=[]
+          let categoriesTabObject=[]
+          for(let i=0; i<data.allCategories.length; i++) {
+          categories.push(data.allCategories[i].name);
+          categoriesTabObject.push(data.allCategories[i]);
+        };
         setCatgoriesTab(categories);
-        console.log(categoriesTab);
+        setCategoriersTabObject(categoriesTabObject);
         })
 }, []);
 
-const filteredOptions = categoriesTab.filter((o) => !selectedItems.includes(o));
+const filteredOptions = categoriesTab.filter((e) => !selectedItems.includes(e));
 
 
+const handleFilterChange = (selectedItems) => {
+    setSelectedItems(selectedItems);
+    props.handleFilterChange(selectedItems)
+}
 
 
 return (
 
-    <Select
-    mode="multiple"
-    placeholder="Filter by category"
-    value={selectedItems}
-    onChange={setSelectedItems}
-    style={{
-      width: '100%',
-    }}
-    options={filteredOptions.map((item) => ({
-      value: item,
-      label: item,
-    }))}
-  />
-
+    <Select mode="multiple" placeholder="Filter by category" value={selectedItems} onChange={handleFilterChange} style={{ width: '100%'}}
+      options={filteredOptions.map((item) => ({ value: item, label: item,}))}
+    />
 )
 
 };
