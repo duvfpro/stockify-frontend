81ffdd78b5ef8b03eec123d13e4f9019cd747048
Kevin <kevin.mavier@hotmail.com>
2023-12-18 12:50:07 +0100

    notif

--- components/Header/Notifications.js
+++ components/Header/Notifications.js
@@ -1,12 +1,39 @@
 import { SmileOutlined } from '@ant-design/icons';
 import { Button, notification } from 'antd';
+import { useEffect, useState } from 'react';
 
 const NotificationButton = () => {
+
+
+  const [dataProducts,setDataProducts] = useState([]);
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch ('http://localhost:3000/products/allProducts');
+      if(!response.ok){
+        throw new Error(`Error: ${response.status}`);
+      }
+      const data = await response.json();
+      setDataProducts(data.allProducts)
+    }
+    catch (error){
+      console.error('Erreur lors du fetch ')
+    }
+  }
+
+  console.log(dataProducts)
+  
+
   const openNotification = () => {
     notification.open({
       message: 'Notifications',
       description: 'Kevin est en rupture de glace',
       className: 'custom-class',
+      duration: 1,
       style: {
         width: 600,
       },
@@ -18,6 +45,8 @@ const NotificationButton = () => {
       )
     });
   };
+
+
 
   return (
     <Button type="primary" onClick={openNotification}>
